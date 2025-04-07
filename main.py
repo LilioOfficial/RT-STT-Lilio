a0b75c85556c5111c3021979e8fda3f7cd6dfb4b
Ubuntu <ubuntu@ip-172-31-3-222.eu-west-3.compute.internal>
2025-04-07 15:17:53 +0000

    m

--- main.py
+++ main.py
@@ -3,48 +3,91 @@ import wave
 import numpy as np
 from faster_whisper import WhisperModel
 import pyaudio
+import torch
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import time 
 
-MODEL = WhisperModel("Systran/faster-whisper-large-v3", device="cpu", compute_type="int8", num_workers=8)
+N_CHUNK = 30
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+MODEL = WhisperModel("Systran/faster-whisper-large-v3", device="cuda" if torch.cuda.is_available() else "cpu", compute_type="int8", num_workers=8)
+
+
 
 def transcribe_chunk(model : WhisperModel, file_path):
     segments, info = model.transcribe(file_path, beam_size=5, language='fr', word_timestamps=True, suppress_blank=True)
     transcription = ' '.join (segment.text for segment in segments)
     return transcription
 
-def record_chunk(p, stream, file_path, chunk_length=1):
-    frames = []
-    for _ in range(0, int (16000 / 1024 * chunk_length)):
-        data = stream.read(1024, exception_on_overflow = False)
-        frames. append (data)
-    wf = wave.open(file_path, 'wb')
-    wf.setnchannels (1)
-    wf.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
-    wf.setframerate (16000) 
-    wf.writeframes(b''.join (frames))
-    wf.close ( )
+
+
+def simulate_audio_stream_n_fixe(file_path):
+    with wave.open(file_path, 'rb') as input_wf:
+        # Get audio parameters from the input file
+        channels = input_wf.getnchannels()
+        sample_width = input_wf.getsampwidth()
+        frame_rate = input_wf.getframerate()
+
+        number_of_frames = input_wf.getnframes()
+
+        print(f"Number of frames: {number_of_frames}")
+
+        number_of_frames = number_of_frames // N_CHUNK
+
+        print(f"Number of chunks: {number_of_frames}")
+        chunk_index = 0
+        while chunk_index < N_CHUNK:
+            # Open the output WAV file with the same parameters
+            chunk_file = f"temp_chunks/chunk_{chunk_index}.wav".format(chunk_index =chunk_index)
+            with wave.open(chunk_file, 'wb') as output_wf:
+                output_wf.setnchannels(channels)
+                output_wf.setsampwidth(sample_width)
+                output_wf.setframerate(frame_rate)
+                index = number_of_frames
+
+                print("Reading and writing audio...")
+                while index > 0:
+                    # Read a chunk of audio data
+                    chunk = input_wf.readframes(CHUNK)
+                    if not chunk:
+                        break
+
+                    # Process the audio chunk
+                    processed_chunk = chunk
+
+                    # Write the processed chunk to the output file
+                    output_wf.writeframes(processed_chunk)
+                    index -= CHUNK
+            transciped = transcribe_chunk(MODEL, chunk_file)
+            print(transciped)
+            chunk_index += 1
+
+    print("Audio processing complete.")
+        # Transcribe the chunk
+        # transcription = transcribe_chunk(MODEL, chunk_file)
+        # print(transcription)
+
+        # Append the new transcription to the accumulated transcription
+        # accumulated_transcription += transcription + " "
+
+        # Remove the temporary chunk file
+        # os.remove(chunk_file)
 
 def main2():
     # Choose your model settings
-   
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
-    accumulated_transcription = "" # Initialize an empty string to accumulate transcriptions
-    try:
-        while True:
-            chunk_file = "temp_chunk.wav"
-            record_chunk(p, stream, chunk_file)
-            transcription = transcribe_chunk(MODEL, chunk_file)
-            print(transcription )
-            os.remove (chunk_file)
-            # Append the new transcription to the accumulated transcription
-            accumulated_transcription += transcription + " "
-    except KeyboardInterrupt:
-        print("Stopping...")
-    # Write the accumulated transcription to the log file
-    with open ("log. txt", "w") as log_file: 
-       log_file.write(accumulated_transcription)
+    print(torch.cuda.is_available())
 
+    start = time.time()
+    # print(transcribe_chunk(MODEL, "./wavFiles/Stade Jean Bouin.wav"))
+    print("Transcription time: ", time.time() - start)
+    # Simulate the audio stream with fixed chunks
+    # simulate_audio_stream_n_fixe("./wavFiles/Stade Jean Bouin.wav")
+    # Simulate the audio stream with silence detection
+    # simulate_audio_stream_with_cutted_silence("./wavFiles/Stade Jean Bouin.wav")
 if __name__ == "__main__":
-    # main2()
     # Start the transcription thread
     print("Starting transcription thread...")
+    main2()
